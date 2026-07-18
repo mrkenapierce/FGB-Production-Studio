511b3cb2d3420ef96fd5b442cb5338706b280ee7
mrkenapierce <mrkenapierce@gmail.com>
2026-07-18 14:28:19 -0500

    Open functional Shorts generator from desktop controls

--- renderer/app-events.js
+++ renderer/app-events.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',function(){
   fgbById('start').onclick=function(){fgbRunning=true;fgbRender()};
   fgbById('pause').onclick=function(){fgbRunning=false;fgbRender()};
   fgbById('reset').onclick=function(){fgbReset()};
-  fgbById('render').onclick=function(){alert('Render MP4 will be connected after the Windows build is stable.')};
+  fgbById('render').onclick=function(){window.location.href='shorts.html'};
   fgbById('preset').onchange=function(){fgbApplyPreset(fgbById('preset').value)};
   fgbById('project').onchange=function(){fgbRender()};
   var fields=['episodeNumber','episodeTitle','business','location','contact','promotion'];
